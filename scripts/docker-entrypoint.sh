77871fd2bbd026e26499065e6086d1561c6946e7
Dirk Moors <dirkmoors@gmail.com>
2015-09-11 11:39:04 +0200

    Seperated commands from entrypoint

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Include
+source "${SCRIPTSDIR}/docker-python-commands.sh"
+
 # Define help message
 show_help() {
     echo """
@@ -10,70 +13,25 @@ Commands
 
 bash    : Start a bash shell
 tox     : Run the Tox tests. Requires 'TOXFILEDIR' and tox to be installed
-shell   : Start a Django Python shell. Requires 'APPDIR' and 'MANAGEFILE'
 python  : Run a Python shell
 help    : Show this message
 """
 }
 
-# Install build dependencies
-install_build_deps(){
-    if [ -f ${DEPLOYMENTDIR}/builddeps.txt ];
-    then
-        echo "Installing build dependencies..."
-        set -e \
-            && buildDeps=`cat ${DEPLOYMENTDIR}/${BUILDDEPSFILE}` \
-            && echo ${buildDeps} \
-            && apt-get update \
-            && apt-get install -y ${buildDeps}
-    fi
-}
-
-# Uninstall build dependencies
-uninstall_build_deps(){
-    if [ -f ${DEPLOYMENTDIR}/builddeps.txt ];
-    then
-        echo "Uninstalling build dependencies..."
-        set -e \
-            && buildDeps=`cat ${DEPLOYMENTDIR}/${BUILDDEPSFILE}` \
-            && echo ${buildDeps} \
-            && rm -rf /var/lib/apt/lists/* \
-            && find /usr/local \
-                \( -type d -a -name test -o -name tests \) \
-                -o \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
-                -exec rm -rf '{}' + \
-            && apt-get purge -y --auto-remove ${buildDeps}
-    fi
-}
-
-# Define manage.py name
-if [ -z $MANAGEFILE ]; then
-    export MANAGEFILE=manage.py
-fi
 
 # Run
 case "$1" in
     bash)
-        /bin/bash "${@:2}"
+        run_bash
     ;;
     tox)
-        trap uninstall_build_deps EXIT INT TERM
-
-        install_build_deps
-
-        echo "Running Tox Tests..."
-        cd ${TOXFILEDIR} \
-            && tox "${@:2}"
-
-        trap - EXIT
-        uninstall_build_deps
-        exit 0
+        run_tox
+    ;;
+    python)
+        run_python
     ;;
     help)
         show_help
-    ;;
-    python)
-        ptipython "${@:2}"
     ;;
     *)
         show_help
---
+++ scripts/docker-python-commands.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Install build dependencies
+install_build_deps(){
+    if [ -f ${DEPLOYMENTDIR}/builddeps.txt ];
+    then
+        echo "Installing build dependencies..."
+        set -e \
+            && buildDeps=`cat ${DEPLOYMENTDIR}/${BUILDDEPSFILE}` \
+            && echo ${buildDeps} \
+            && apt-get update \
+            && apt-get install -y ${buildDeps}
+    fi
+}
+
+# Uninstall build dependencies
+uninstall_build_deps(){
+    if [ -f ${DEPLOYMENTDIR}/builddeps.txt ];
+    then
+        echo "Uninstalling build dependencies..."
+        set -e \
+            && buildDeps=`cat ${DEPLOYMENTDIR}/${BUILDDEPSFILE}` \
+            && echo ${buildDeps} \
+            && rm -rf /var/lib/apt/lists/* \
+            && find /usr/local \
+                \( -type d -a -name test -o -name tests \) \
+                -o \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
+                -exec rm -rf '{}' + \
+            && apt-get purge -y --auto-remove ${buildDeps}
+    fi
+}
+
+# Run bash
+run_bash(){
+    /bin/bash "${@:2}"
+}
+
+# Run tox
+run_tox(){
+    if [ -f ${TOXFILEDIR}/tox.ini ];
+    then
+        trap uninstall_build_deps EXIT INT TERM
+
+        install_build_deps
+
+        echo "Running Tox Tests..."
+        cd ${TOXFILEDIR} \
+            && tox "${@:2}"
+
+        trap - EXIT
+        uninstall_build_deps
+        exit 0
+    else
+        echo "Could not find tox.ini!"
+        exit 1
+    fi
+}
+
+# Run python
+run_python(){
+    ptipython "${@:2}"
+}
